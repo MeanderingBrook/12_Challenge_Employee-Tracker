0baebf297f8e6034e7914beb00a7ce294f998ed9
Meandering Brook <jaguar_rebuke_0g@icloud.com>
2024-08-24 15:07:40 -0400

    Query Structure, Select All

--- archive/archive.js
+++ archive/archive.js
@@ -110,3 +110,80 @@
 
 // index.js
 const { getPool } = require("./db/connect.js");
+
+//   const userInput = await inquirer.prompt(mainMenu);
+//   console.log(`index.js Line 49: ${userInput.menuOptions}`);
+//   switch (userInput.menuOptions) {
+//     case "View All Employees":
+//       console.log("blah blah Output All Employees");
+//       viewEmpTable();
+//       break;
+//     case "Add New Employee":
+//       console.log("blah blah Add New Employee");
+//       addEmp();
+//       break;
+//     case "Update Employee Role":
+//       console.log("blah blah Update Employee Role");
+//       break;
+//     case "View All Roles":
+//       console.log("blah blah View All Roles");
+//       viewRoleTable();
+//       break;
+//     case "Add New Role":
+//       console.log("blah blah Add New Role");
+//       addRole();
+//       break;
+//     case "View All Departments":
+//       console.log("blah blah View All Departments");
+//       break;
+//     case "Add New Department":
+//       console.log("blah blah Add New Department");
+//       break;
+//     case "Quit Application":
+//       console.log("blah blah Quit Application");
+//       break;
+//   }
+
+// const mainMenu = [
+//   {
+//     type: "list",
+//     name: "menuOptions",
+//     message: "What would  you like to do?",
+//     choices: [
+//       "View All Employees",
+//       "Add New Employee",
+//       "Update Employee Role",
+//       "View All Roles",
+//       "Add New Role",
+//       "View All Departments",
+//       "Add New Department",
+//       "Quit Application",
+//     ],
+//   },
+// ];
+
+// async function selectEmpTable() {
+//   const connection = await pool.connect();
+//   try {
+//     // await pool.connect();
+//     console.log("query.js Line 194: Connected to Database (selectEmpTable)");
+
+//     const res = await pool.query(`
+//       SELECT * FROM employee;
+//       `);
+//     console.log("query.js Line 198: Queried data from Table (selectEmpTable)");
+//     return res.rows;
+//   } catch (err) {
+//     console.log(
+//       "query.js Line 201: Error retrieving Table (selectEmpTable)",
+//       err
+//     );
+//   } finally {
+//     // await pool.end();
+//     console.log(
+//       "query.js Line 207: Closed connection to Database (selectEmpTable)"
+//     );
+//     // pool.release();
+//     connection.release();
+//   }
+// }
--- db/query.js
+++ db/query.js
@@ -1,6 +1,8 @@
 // Require Application Modules
 const pool = require("./connect.js");
 
+// Deprecated in favor Database Creation through Postgres Shell
+// Creates Employee Database (employee_db)
 async function dbCreate() {
   // const client = await getPool();
   await pool.connect();
@@ -14,6 +16,8 @@ async function dbCreate() {
   await client.end();
 }
 
+// Deprecated in favor of dedicated Connection through connect.js
+// Connects to newly-created Employee Database (employee_db)
 async function dbConnect() {
   // const client = await getPool();
   await pool.connect();
@@ -26,6 +30,7 @@ async function dbConnect() {
   await client.end();
 }
 
+// Creates Employee Database, Department Table (invoked once, to initialize App)
 async function createDeptTable() {
   const connection = await pool.connect();
   try {
@@ -34,7 +39,7 @@ async function createDeptTable() {
 
     // await pool.query(`
     await connection.query(`
-      CREATE TABLE department (
+      CREATE TABLE IF NOT EXISTS department (
       id SERIAL PRIMARY KEY,
       dept_name VARCHAR(30) UNIQUE NOT NULL
       );
@@ -50,22 +55,25 @@ async function createDeptTable() {
     console.log(
       "query.js Line 43: Closed connection to Database (createDeptTable)"
     );
-    // pool.release();
     connection.release();
   }
 }
 
+// Creates Employee Database, Role Table (invoked once, to initialize App)
 async function createRoleTable() {
+  const connection = await pool.connect();
   try {
-    await pool.connect();
+    // await pool.connect();
     console.log("query.js Line 57: Connected to Database (createRoleTable)");
 
-    await pool.query(`
-      CREATE TABLE role (
+    // await pool.query(`
+    await connection.query(`
+      CREATE TABLE IF NOT EXISTS role (
       id SERIAL PRIMARY KEY,
       title VARCHAR(30) UNIQUE NOT NULL,
       salary DECIMAL,
-      department INTEGER
+      department INTEGER,
+      CONSTRAINT fk_department FOREIGN KEY (department) REFERENCES department(id) ON DELETE CASCADE
       );
     `);
     console.log("query.js Line 67: Created new Table (createRoleTable)");
@@ -79,24 +87,33 @@ async function createRoleTable() {
     console.log(
       "query.js Line 76: Closed connection to Database (createRoleTable)"
     );
-    pool.release();
+    connection.release();
   }
 }
 
+// Creates Employee Database, Employee Table (invoked once, to initialize App)
 async function createEmpTable() {
+  const connection = await pool.connect();
   try {
-    await pool.connect();
+    // await pool.connect();
     console.log("query.js Line 82: Connected to Database (createEmpTable)");
 
-    await pool.query(`
-      CREATE TABLE employee (
+    // await pool.query(`
+    await connection.query(`
+      CREATE TABLE IF NOT EXISTS employee (
       id SERIAL PRIMARY KEY,
       first_name VARCHAR(30) NOT NULL,
       last_name VARCHAR(30) NOT NULL,
-      role_id INTEGER,
-      manager_id INTEGER
+      role_id INTEGER NOT NULL,
+      manager_id INTEGER,
+      CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+      CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
       );
     `);
+    // CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
+    // CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    // CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+    // CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     console.log("query.js Line 93: Created new Table (createEmpTable)");
   } catch (err) {
     console.log("query.js Line 95: Error creating Table (createEmpTable)", err);
@@ -105,21 +122,23 @@ async function createEmpTable() {
     console.log(
       "query.js Line 99: Closed connection to Database (createEmpTable)"
     );
-    pool.release();
+    connection.release();
   }
 }
 
+// Seeds Employee Database, Department Table (invoked once, to initialize App)
 async function seedDeptTable() {
+  const connection = await pool.connect();
   try {
-    await pool.connect();
+    // await pool.connect();
     console.log("query.js Line 53: Connected to Database (seedDeptTable)");
 
     await pool.query(`
-      INSERT INTO department (id, dept_name)
-      VALUES  (1, 'Sales'),
-              (2, 'Engineering'),
-              (3, 'Finance'),
-              (4, 'Legal');
+      INSERT INTO department (dept_name)
+      VALUES  ('Sales'),
+              ('Engineering'),
+              ('Finance'),
+              ('Legal');
       `);
     console.log("query.js Line 62: Populated data to Table (seedDeptTable)");
   } catch (err) {
@@ -132,25 +151,28 @@ async function seedDeptTable() {
     console.log(
       "query.js Line 67: Closed connection to Database (seedDeptTable)"
     );
-    pool.release();
+    connection.release();
   }
 }
 
+// Seeds Employee Database, Role Table (invoked once, to initialize App)
 async function seedRoleTable() {
+  const connection = await pool.connect();
   try {
-    await pool.connect();
+    // await pool.connect();
     console.log("query.js Line 133: Connected to Database (seedRoleTable)");
 
-    await pool.query(`
-      INSERT INTO role (id, title, salary, department)
-      VALUES  (1, 'Sales Lead', 10000, 1),
-              (2, 'Salesperson', 80000, 1),
-              (3, 'Lead Engineer', 150000, 2),
-              (4, 'Software Engineer', 120000, 2),
-              (5, 'Account Manager', 160000, 3),
-              (6, 'Accountant', 125000, 3),
-              (7, 'Legal Team Lead', 250000, 4),
-              (8, 'Lawyer', 190000, 4);
+    // await pool.query(`
+    await connection.query(`
+      INSERT INTO role (title, salary, department)
+      VALUES  ('Sales Lead', 10000, 1),
+              ('Salesperson', 80000, 1),
+              ('Lead Engineer', 150000, 2),
+              ('Software Engineer', 120000, 2),
+              ('Account Manager', 160000, 3),
+              ('Accountant', 125000, 3),
+              ('Legal Team Lead', 250000, 4),
+              ('Lawyer', 190000, 4);
       `);
     console.log("query.js Line 142: Populated data to Table (seedRoleTable)");
   } catch (err) {
@@ -163,25 +185,28 @@ async function seedRoleTable() {
     console.log(
       "query.js Line 151: Closed connection to Database (seedRoleTable)"
     );
-    pool.release();
+    connection.release();
   }
 }
 
+// Seeds Employee Database, Employee Table (invoked once, to initialize App)
 async function seedEmpTable() {
+  const connection = await pool.connect();
   try {
-    await pool.connect();
+    // await pool.connect();
     console.log("query.js Line 163: Connected to Database (seedEmpTable)");
 
-    await pool.query(`
-      INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-      VALUES  (1, 'Aaron', 'Anderson', 1, NULL),
-              (2, 'Brian', 'Black', 2, 1),
-              (3, 'Carl', 'Carpenter', 3, NULL),
-              (4, 'David', 'Draper', 4, 3),
-              (5, 'Erin', 'Eckles', 5, NULL),
-              (6, 'Frank', 'Fuller', 6, 5),
-              (7, 'George', 'Green', 7, 0),
-              (8, 'Harry', 'Hamlin', 8, 7);
+    // await pool.query(`
+    await connection.query(`
+      INSERT INTO employee (first_name, last_name, role_id, manager_id)
+      VALUES  ('Aaron', 'Anderson', 1, NULL),
+              ('Brian', 'Black', 2, 1),
+              ('Carl', 'Carpenter', 3, NULL),
+              ('David', 'Draper', 4, 3),
+              ('Erin', 'Eckles', 5, NULL),
+              ('Frank', 'Fuller', 6, 5),
+              ('George', 'Green', 7, NULL),
+              ('Harry', 'Hamlin', 8, 7);
       `);
     console.log("query.js Line 176: Populated data to Table (seedEmpTable)");
   } catch (err) {
@@ -194,18 +219,38 @@ async function seedEmpTable() {
     console.log(
       "query.js Line 185: Closed connection to Database (seedEmpTable)"
     );
-    pool.release();
+    connection.release();
   }
 }
 
+// Seleects all records from Employee Table
 async function selectEmpTable() {
+  const connection = await pool.connect();
   try {
-    await pool.connect();
+    // await pool.connect();
     console.log("query.js Line 194: Connected to Database (selectEmpTable)");
 
     const res = await pool.query(`
-      SELECT * FROM employee;
+      SELECT
+        employee.first_name || ' ' || employee.last_name employee_name,
+        employee.role_id,
+        role.title,
+        role.salary,
+        employee.manager_id,
+        employee.first_name || ' ' || employee.last_name manager_name,
+        department.dept_name
+      FROM 
+        employee
+      LEFT JOIN
+        role ON employee.role_id = role.id
+      LEFT JOIN
+        department ON role.department = department.id;
       `);
+    // LEFT JOIN
+    // employee ON manager.id = employee.manager_id;
+    // LEFT JOIN
+    //   employee on manager_id = id;
+    // employee on employee.manager_id = employee.id;
     console.log("query.js Line 198: Queried data from Table (selectEmpTable)");
     return res.rows;
   } catch (err) {
@@ -218,15 +263,40 @@ async function selectEmpTable() {
     console.log(
       "query.js Line 207: Closed connection to Database (selectEmpTable)"
     );
-    pool.release();
+    connection.release();
   }
 }
 
-async function newEmp() {}
+// Seleects all records from Department Table
+async function selectDeptTable() {
+  const connection = await pool.connect();
+  console.log("Connection: ", connection);
 
-async function selectRoleTable() {
   try {
-    await pool.connect();
+    console.log("query.js Line 231: Connected to Database (selectDeptTable)");
+
+    const res = await pool.query(`
+      SELECT * FROM department;
+      `);
+    console.log("query.js Line 236: Queried data from Table (selectDeptTable)");
+    console.log("Data: ", res.rows);
+    return res.rows;
+  } catch (err) {
+    console.log("query.js Line 239: Error retrieving Table (selectDeptTable)"),
+      err;
+  } finally {
+    console.log(
+      "query.js Line 242: Closed connection to Database (selectDeptTable)"
+    );
+    connection.release();
+  }
+}
+
+// Seleects all records from Role Table
+async function selectRoleTable() {
+  const connection = await pool.connect();
+  try {
+    // await pool.connect();
     console.log("query.js Line 218: Connected to Database (selectRoleTable)");
 
     const res = await pool.query(`
@@ -241,9 +311,11 @@ async function selectRoleTable() {
     console.log(
       "query.js Line 230: Closed connection to Database (selectRoleTable)"
     );
-    pool.release();
+    connection.release();
   }
 }
+
+async function newEmp() {}
 
 module.exports = {
   dbCreate,
@@ -256,5 +328,6 @@ module.exports = {
   seedEmpTable,
   selectEmpTable,
   newEmp,
+  selectDeptTable,
   selectRoleTable,
 };
